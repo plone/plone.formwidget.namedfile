ba9da3f6ae446a61bc9c23d41030b5ee5e7ff212
Martijn Pieters <plone.org@zopatista.com>
2011-01-21 15:51:48 +0000

    Deal with UTF-8 filenames until https://bugs.launchpad.net/zope2/+bug/499696 is fixed.
    Fixes http://code.google.com/p/dexterity/issues/detail?id=148
    
    svn path=/plone.formwidget.namedfile/trunk/; revision=46983

--- plone/formwidget/namedfile/converter.py
+++ plone/formwidget/namedfile/converter.py
@@ -29,8 +29,10 @@ class NamedDataConverter(BaseDataConverter):
             headers = value.headers
             filename = safe_basename(value.filename)
             
-            if filename is not None:
-                filename = unicode(filename)
+            if filename is not None and not isinstance(filename, unicode):
+                # Work-around for
+                # https://bugs.launchpad.net/zope2/+bug/499696
+                filename = filename.decode('utf-8')
             
             contentType = 'application/octet-stream'
             if headers:
