d42f9a240b5e146ee9b0af17c87947a44659ffbd
Jamie Lentin <jm+plone@lentin.co.uk>
2011-03-01 17:41:21 +0000

    Use what the parent considers to be the widget name if available.
    svn path=/plone.formwidget.namedfile/trunk/; revision=47844

--- plone/formwidget/namedfile/widget.py
+++ plone/formwidget/namedfile/widget.py
@@ -73,9 +73,20 @@ class NamedFileWidget(Explicit, file.FileWidget):
         if self.ignoreContext:
             return None
         if self.filename_encoded:
-            return "%s/++widget++%s/@@download/%s" % (self.request.getURL(), self.field.__name__, self.filename_encoded)
+            return "%s/++widget++%s/@@download/%s" % (self.request.getURL(), self._widget_name(), self.filename_encoded)
         else:
-            return "%s/++widget++%s/@@download" % (self.request.getURL(), self.field.__name__)
+            return "%s/++widget++%s/@@download" % (self.request.getURL(), self._widget_name())
+
+    def _widget_name(self):
+        # self has the fully qualified name, including 'form.widgets.'
+        # Search one level up to find the shorter form
+        if not(getattr(self,'__parent__',None)):
+            return self.field.getName()
+
+        possible_names = [k for k,v in self.__parent__.items() if v is self]
+        if len(possible_names) != 1:
+            raise ValueError("Didn't find a single possible widget, found %d" % len(possible_names))
+        return possible_names[0]
     
     def action(self):
         action = self.request.get("%s.action" % self.name, "nochange")
