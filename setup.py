8c200e1bf98fd59fcb2da865b779f983bda63964
Maurits van Rees <maurits@vanrees.org>
2023-03-14 02:08:25 +0100

    Add python_requires.
    [ci skip]

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     namespace_packages=["plone", "plone.formwidget"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         "setuptools",
         "plone.base",
